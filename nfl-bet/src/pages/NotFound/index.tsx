1fa2b9bd71cc085dad00a06aa92f32f6c6bab394
Dário Teodoro <dario.teodoro@live.com>
2022-01-10 15:25:37 -0300

    Add NotFound page

---
+++ nfl-bet/src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const NotFound: React.FC = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
