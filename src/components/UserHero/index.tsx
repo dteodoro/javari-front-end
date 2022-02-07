aad2126dbd9cf4af83ff9b0fe552310f9d76c6dc
Dário Teodoro <dario.teodoro@live.com>
2022-02-07 08:57:17 -0300

    new component UserHero

---
+++ src/components/UserHero/index.tsx
@@ -0,0 +1,21 @@
+import { Avatar, Box, Typography } from "@mui/material";
+import { IPlayer } from "../../types/player";
+import style from "./styles.module.scss";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+interface Props {
+  player: IPlayer;
+}
+const UserHero = ({ player }: Props) => {
+  return (
+    <Box className={style.root}>
+      <Avatar variant="circular" className={style.avatar}>
+        <AccountCircleIcon />
+      </Avatar>
+      <Typography variant="h6">{player.fullName}</Typography>
+      <Typography variant="subtitle2">{player.position}Th</Typography>
+    </Box>
+  );
+};
+
+export default UserHero;
