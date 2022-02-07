c58e29c70b942e6b3f5eeab1819a7d8f66bf0af0
Dário Teodoro <dario.teodoro@live.com>
2022-02-07 08:54:24 -0300

    new component RecordCard

---
+++ src/components/RecordCard/index.tsx
@@ -0,0 +1,50 @@
+import { Box, Card, Divider, Typography } from "@mui/material";
+
+import style from "./styles.module.scss";
+import { RANK_STATUS } from "../../types/constants";
+import RankStatus from "../RankStatus";
+
+interface Props {
+  rank: RANK_STATUS;
+}
+
+const RecordCard = ({ rank }: Props) => {
+  return (
+    <Card className={style.cardRecord__root}>
+      <Box className={style.cardRecord__pointsContainer}>
+        <RankStatus rankStatus={rank} />
+        <Box className={style.cardRecord__points}>
+          <Typography variant="h3">48</Typography>
+          <Typography ml={1} variant="subtitle2">
+            Pts
+          </Typography>
+        </Box>
+      </Box>
+      <Divider
+        orientation="vertical"
+        variant="middle"
+        flexItem
+        className={style.cardRecord__divider}
+      />
+      <Box className={style.cardRecord__statistics}>
+        <Box className={style.cardRecord__efficiency}>
+          <Typography variant="subtitle2">Hits</Typography>
+          <Typography variant="h6">25/30</Typography>
+        </Box>
+        <Box className={style.cardRecord__efficiency}>
+          <Typography mr={1} variant="subtitle2">
+            EFF
+          </Typography>
+          <Box sx={{ display: "ruby" }}>
+            <Typography variant="h4">98</Typography>
+            <Typography ml={0} variant="body2">
+              %
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Card>
+  );
+};
+
+export default RecordCard;
