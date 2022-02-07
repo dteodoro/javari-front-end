3753222ab1fb22a793c31e2912f0be68aacf469c
Dário Teodoro <dario.teodoro@live.com>
2022-02-07 08:54:04 -0300

    New Component RankStatus

---
+++ src/components/RankStatus/index.tsx
@@ -0,0 +1,29 @@
+import { Box } from "@mui/material";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+
+import { RANK_STATUS } from "../../types/constants";
+import style from "./styles.module.scss";
+
+interface Props {
+  rankStatus: RANK_STATUS;
+}
+
+const RankStatus = ({ rankStatus }: Props) => {
+  return (
+    <Box className={style.cardRecord__rankIcon}>
+      {rankStatus === RANK_STATUS.UP && (
+        <ArrowCircleUpIcon color="success" sx={{ marginTop: "12px" }} />
+      )}
+      {rankStatus === RANK_STATUS.DOWN && (
+        <ArrowCircleDownIcon color="error" sx={{ marginTop: "12px" }} />
+      )}
+      {rankStatus === RANK_STATUS.NO_CHANGE && (
+        <RemoveCircleOutlineIcon color="secondary" sx={{ marginTop: "12px" }} />
+      )}
+    </Box>
+  );
+};
+
+export default RankStatus;
