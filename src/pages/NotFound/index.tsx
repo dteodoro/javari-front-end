79482711a277078b5381650b0b7e0d8188543756
Dário Teodoro <dario.teodoro@live.com>
2022-02-07 08:56:51 -0300

    refactory css

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Container } from "@mui/material";
 
 import style from ".styles.module.scss";
 
 const NotFound: React.FC = () => {
   return (
-    <div>
+    <Container>
       <h1>Not Found</h1>
-    </div>
+    </Container>
   );
 };
 
--- src/pages/Teams/index.tsx
+++ src/pages/Teams/index.tsx
@@ -1,12 +1,115 @@
-import React from "react";
+import {
+  Box,
+  Container,
+  Divider,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 
+import DivisionContainer from "../../containers/DivisionContainer";
+import { CONFERENCES, DIVISIONS } from "../../types/constants";
+import { IDivision } from "../../types/division";
+import { ITeam } from "../../types/team";
 import style from "./styles.module.scss";
+
+const teamData: ITeam[] = [
+  {
+    id: 1,
+    logo: "/nfl.svg",
+    name: "Raiders",
+    nickName: "Las Vegas",
+    displayName: "Las Vegas Raiders",
+    abbreviation: "LV",
+    stats: "(2-3-1)",
+  },
+  {
+    id: 2,
+    logo: "/nfl.svg",
+    name: "Raiders",
+    nickName: "Las Vegas",
+    displayName: "Las Vegas Raiders",
+    abbreviation: "LV",
+    stats: "(2-3-1)",
+  },
+  {
+    id: 3,
+    logo: "/nfl.svg",
+    name: "Raiders",
+    nickName: "Las Vegas",
+    displayName: "Las Vegas Raiders",
+    abbreviation: "LV",
+    stats: "(2-3-1)",
+  },
+  {
+    id: 4,
+    logo: "/nfl.svg",
+    name: "Raiders",
+    nickName: "Las Vegas",
+    displayName: "Las Vegas Raiders",
+    abbreviation: "LV",
+    stats: "(2-3-1)",
+  },
+];
+
+const divisions: IDivision[] = [
+  {
+    name: "AFC North",
+    teams: teamData,
+  },
+  {
+    name: "AFC Lest",
+    teams: teamData,
+  },
+  {
+    name: "AFC Sul",
+    teams: teamData,
+  },
+  {
+    name: "AFC Oest",
+    teams: teamData,
+  },
+];
 
 const Teams = () => {
   return (
-    <div>
-      <h1>Todos os Times Aqui</h1>
-    </div>
+    <Container>
+      <Box className={style.filterBar}>
+        <Typography
+          fontWeight="bold"
+          variant="h6"
+          className={style.filterBarTitle}
+        >
+          NFL TEAMS
+        </Typography>
+        <Box className={style.selectsGroup}>
+          <Select
+            className={style.selectItem}
+            // onChange={handleChangeYear}
+            // value={year}
+          >
+            {CONFERENCES.map((c) => (
+              <MenuItem key={c} value={c}>
+                {c}
+              </MenuItem>
+            ))}
+          </Select>
+          <Select
+            className={`${style.selectItem} ${style.lastSelect}`}
+            // onChange={handleChangeWeek}
+            // value={week}
+          >
+            {DIVISIONS.map((d) => (
+              <MenuItem key={d} value={d}>
+                {d}
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      </Box>
+      <Divider />
+      <DivisionContainer divisions={divisions} />
+    </Container>
   );
 };
 
