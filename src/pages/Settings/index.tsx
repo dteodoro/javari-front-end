411421d056e0128e6c030cc60445a8ec74d69302
Dário <dario.teodoro@live.com>
2023-03-22 23:36:29 +0000

    Create Setting Page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,9 +1,49 @@
+import { Box, Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import React from "react";
 
-// import { Container } from './styles';
+import style from "./styles.module.scss";
+import api from "../../services/api";
 
 const Settings: React.FC = () => {
-  return <h1>Configurações</h1>;
+  const [loadingSchedule, setLoadingSchedule] = React.useState(false);
+  const [loadingTeam, setLoadingTeam] = React.useState(false);
+
+  async function handleScheduleClick() {
+    setLoadingSchedule(true);
+    await api.get("/loader/schedules");
+    setLoadingSchedule(false);
+  }
+  async function handleTeamClick() {
+    setLoadingTeam(true);
+    await api.get("/loader/teams");
+    setLoadingTeam(false);
+  }
+  return (
+    <Box>
+      <Typography variant="h4">Configurações</Typography>
+
+      <Box mr={2} mt={2} className={style.buttonContainer}>
+        <Typography variant="h5">Loaders</Typography>
+        <LoadingButton
+          onClick={handleScheduleClick}
+          loading={loadingSchedule}
+          variant="outlined"
+          className={style.btnItem}
+        >
+          <span>Load Schedules</span>
+        </LoadingButton>
+        <LoadingButton
+          onClick={handleTeamClick}
+          loading={loadingTeam}
+          variant="outlined"
+          className={style.btnItem}
+        >
+          <span>Load Teams</span>
+        </LoadingButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Settings;
