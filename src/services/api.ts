94ad3afbc02e8bc9aaf0f8bce658129771f0a5fe
Dário <dario.teodoro@live.com>
2023-09-03 18:27:13 +0100

    Api address by ENV

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_BACKEND_BASE_API,
   withCredentials: false,
   headers: {
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": `${process.env.REACT_APP_SERVER_HOST}:3000`,
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     "Access-Control-Allow-Headers": "*",
   },
