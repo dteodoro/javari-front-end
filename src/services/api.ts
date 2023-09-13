c7123992e5749d0e8cf3d3a7c2e2d8740ef13656
Dário <dario.teodoro@live.com>
2023-09-13 17:47:20 +0100

    cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_API,
   withCredentials: false,
   headers: {
-    "Access-Control-Allow-Origin": `http://javari.ddns.net/`,
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     "Access-Control-Allow-Headers": "*",
   },
