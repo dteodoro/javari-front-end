02ab716fa3bc03267b3c87ecca48e40add149376
Dário <dario.teodoro@live.com>
2023-09-13 17:39:49 +0100

    fix cross

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_API,
   withCredentials: false,
   headers: {
-    "Access-Control-Allow-Origin": `${process.env.REACT_APP_SERVER_HOST}:3000`,
+    "Access-Control-Allow-Origin": `http://javari.ddns.net/`,
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     "Access-Control-Allow-Headers": "*",
   },
