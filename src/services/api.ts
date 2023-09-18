cff508617e076182fffb1cbf079d7f221b16f8df
Dário Teodoro <dario.teodoro@live.com>
2023-09-18 17:32:59 +0100

    Update api.ts
    add https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://javari.ddns.net:8080",
+  baseURL: "https://javari.ddns.net:8080",
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
