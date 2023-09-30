1a6f9aad7ce507bc75360c51c5ea210e57f37501
Dário <dario.teodoro@live.com>
2023-09-30 20:47:27 +0100

    Fix: Adjust AppBar space

--- src/containers/BetCardContainer/index.tsx
+++ src/containers/BetCardContainer/index.tsx
@@ -9,7 +9,7 @@ interface BetCardProps {
 
 const BetCardContainer: React.FC<BetCardProps> = ({ data }) => {
   return (
-    <Box p={0} m={0}>
+    <Box p={0} m={0} mb={2}>
       <Grid container spacing={2}>
         {data?.map((schedule) => (
           <Grid key={schedule.id} item xs={12} sm={6} md={6} lg={4} xl={4}>
