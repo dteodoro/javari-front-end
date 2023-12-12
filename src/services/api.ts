808463bf7cc082f98661c12363953a24e0d8f634
Dário <dario.teodoro@live.com>
2023-12-12 09:50:27 -0300

    change ddns

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://roles.fun:8080",
+  baseURL: "http://javari.ddns.net:8080",
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
