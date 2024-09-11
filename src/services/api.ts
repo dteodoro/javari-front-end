9b054c5c950bf95a6cbebd12a7f8e468e28b133d
Dário <dario.teodoro@neogrid.com>
2024-09-10 23:23:13 -0300

    adjust hostnames for docker

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://darioteodoro.dev.br:8080",
+  baseURL: "http://javari-gateway",
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
